e2a17cfd3427e4916e82b415460d13416f735317
Evan Murray <evanmurray2@gmail.com>
2019-02-18 12:50:40 -0800

    onto more debugging

---
+++ tests/synch_1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
+#include <pthread.h>
+
+
+sem_t mutex;
+
+void* thread(void* arg)
+{
+    //wait
+    sem_wait(&mutex);
+    printf("\nEntered..\n");
+
+    //critical section
+    printf("CRITICAL SECTION\n");
+    sleep(4);
+
+    //signal
+    printf("\nJust Exiting...\n");
+    sem_post(&mutex);
+}
+
+
+int main()
+{
+    sem_init(&mutex, 0, 1);
+    pthread_t t1,t2;
+    pthread_create(&t1,NULL,thread,NULL);
+    sleep(2);
+    pthread_create(&t2,NULL,thread,NULL);
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    sem_destroy(&mutex);
+    return 0;
+}
