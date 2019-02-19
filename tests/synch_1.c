abbc0fd260a5fc1d8bbe483f6e6ac62c59c57499
Evan Murray <evanmurray2@gmail.com>
2019-02-18 22:38:57 -0800

    read TODO's for more work

--- tests/synch_1.c
+++ tests/synch_1.c
@@ -30,7 +30,6 @@ int main()
     pthread_create(&t1,NULL,thread,NULL);
     sleep(2);
     pthread_create(&t2,NULL,thread,NULL);
-    while(true);
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
     sem_destroy(&mutex);
