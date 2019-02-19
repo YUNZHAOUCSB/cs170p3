6a3b05323ac4aff70d6658aeacc5b034c983d602
Evan Murray <evanmurray2@gmail.com>
2019-02-18 22:38:26 -0800

    done

--- tests/synch_1.c
+++ tests/synch_1.c
@@ -10,7 +10,7 @@ sem_t mutex;
 void* thread(void* arg)
 {
     //wait
-    //sem_wait(&mutex);
+    sem_wait(&mutex);
     printf("\nEntered..\n");
 
     //critical section
@@ -19,19 +19,20 @@ void* thread(void* arg)
 
     //signal
     printf("\nJust Exiting...\n");
-    //sem_post(&mutex);
+    sem_post(&mutex);
 }
 
 
 int main()
 {
-    //sem_init(&mutex, 0, 1);
+    sem_init(&mutex, 0, 1);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,thread,NULL);
     sleep(2);
     pthread_create(&t2,NULL,thread,NULL);
+    while(true);
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
-    //sem_destroy(&mutex);
+    sem_destroy(&mutex);
     return 0;
 }
