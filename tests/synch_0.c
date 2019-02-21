03394a43ece0d74af4924eb77070a75c4ed97e2d
Evan Murray <evanmurray2@gmail.com>
2019-02-21 14:02:50 -0800

    changing test 0 to test for return value

--- tests/synch_0.c
+++ tests/synch_0.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
+#include <unistd.h>
 #include <pthread.h>
 
-// A normal C function that is executed as a thread  
-// when its name is specified in pthread_create() 
-int myThreadFun(void *vargp)
+int thread(void *vargp)
 {
     sleep(1);
-    printf("Printing GeeksQuiz from Thread \n");
+    printf("In Thread! \n");
     return 57;
 }
 
@@ -16,7 +14,7 @@ int main() {
     pthread_t thread_id;
     void *returnValue;
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, (void * (*)(void*)) myThreadFun, NULL);
+    pthread_create(&thread_id, NULL, (void * (*)(void*)) thread, NULL);
     pthread_join(thread_id, &returnValue);
     printf("After Thread\n");
     printf("return value: %d\n",  (int)returnValue);
