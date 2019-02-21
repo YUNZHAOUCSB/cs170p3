cb0fc0eef7b96d4d2aa5ca3d4a08dd27f7428816
Evan Murray <evanmurray2@gmail.com>
2019-02-21 14:01:13 -0800

    changing test 0 to test for return value

--- tests/synch_0.c
+++ tests/synch_0.c
@@ -5,18 +5,20 @@
 
 // A normal C function that is executed as a thread  
 // when its name is specified in pthread_create() 
-void *myThreadFun(void *vargp)
+int myThreadFun(void *vargp)
 {
     sleep(1);
     printf("Printing GeeksQuiz from Thread \n");
-    return NULL;
+    return 57;
 }
 
 int main() {
     pthread_t thread_id;
+    void *returnValue;
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, myThreadFun, NULL);
-    pthread_join(thread_id, NULL);
+    pthread_create(&thread_id, NULL, (void * (*)(void*)) myThreadFun, NULL);
+    pthread_join(thread_id, &returnValue);
     printf("After Thread\n");
+    printf("return value: %d\n",  (int)returnValue);
     exit(0);
 }
